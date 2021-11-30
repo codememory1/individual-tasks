f426ede710f3bfce17c57d0f9b4fe93bf85940f5
Danil <kostynd1@gmail.com>
2021-12-01 00:55:48 +0200

    Исправление ошибок релиза

--- Commands/WorkerCommand.php
+++ Commands/WorkerCommand.php
@@ -1,16 +1,12 @@
 <?php
-declare(ticks = 1);
+declare(ticks=1);
 
 namespace Codememory\Components\IndividualTasks\Commands;
 
-use Codememory\Components\Console\Command;
 use Codememory\Components\Database\Orm\Commands\AbstractCommand;
 use Codememory\Components\Database\Pack\DatabasePack;
-use Codememory\Components\Environment\Environment;
 use Codememory\Components\IndividualTasks\Worker;
-use Codememory\FileSystem\File;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
@@ -31,27 +27,13 @@ class WorkerCommand extends AbstractCommand
     /**
      * @inheritDoc
      */
-    protected ?string $description = '';
-
-    /**
-     * @inheritDoc
-     */
-    protected function wrapArgsAndOptions(): Command
-    {
-
-        $this->addOption('failed', 'f', InputOption::VALUE_NONE, 'Restart jobs that were not executed');
-
-        return $this;
-
-    }
+    protected ?string $description = 'Start a daemon to track all tasks';
 
     /**
      * @inheritDoc
      */
     protected function handler(InputInterface $input, OutputInterface $output): int
     {
-
-        Environment::__constructStatic(new File());
 
         $worker = new Worker(new DatabasePack($this->connection));
 
